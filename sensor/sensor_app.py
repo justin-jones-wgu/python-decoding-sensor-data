f0efd2d60a14e1d8eb5da6fce5db43f34b2fa4e9
Justin Jones <justin.jones@wgu.edu>
2022-11-07 09:02:45 -0600

    fix: fixed the string formatting

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -13,15 +13,18 @@ print("Sensor Data App")
 # Module 1 code here:
 data = load_sensor_data()
 print("Loaded records: {}".format(len(data)))
+
+# Module 2 code here:
 house_info = HouseInfo(data)
 test_area = 1
 recs = house_info.get_data_by_area("id", rec_area=test_area)
-test_date = datetime.strptime("5/9/20", "%m/%d/%y")
 print("\nHouse sensor records for area {} = {}".format(test_area, len(recs)))
-recs = house_info.get_data_by_date("id", rec_data=test_date)
-print("House sensor records for date: {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
 
-# Module 2 code here:
+test_date = datetime.strptime("5/9/20", "%m/%d/%y")
+recs = house_info.get_data_by_date("id", rec_data=test_date)
+print("House sensor records for date {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
+
+
 
 # Module 3 code here:
 
