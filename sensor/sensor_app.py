3d0b2ad05871c3c9139b59dcbabd30de2186a8d5
Justin Jones <justin.jones@wgu.edu>
2022-11-08 08:16:34 -0600

    feat: added module 5 code

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -3,6 +3,9 @@ from load_data import load_sensor_data
 from house_info import HouseInfo
 from datetime import datetime, date
 from temperature_info import TemperatureData
+from humidity_info import HumidityData
+from statistics import mean
+
 ##############################
 
 ##############################
@@ -44,4 +47,13 @@ print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs), min(recs)))
 
 # Module 4 code here:
 
+humidity_data = HumidityData(data)
+recs = humidity_data.get_data_by_area(rec_area=test_area)
+
+print("\nHouse Humidity sensor records for area {} = {}".format(test_area, len(recs)))
+print("\tAverage: {} humidity".format(mean(recs)))
+
+recs = humidity_data.get_data_by_date(rec_date=test_date)
+print("House Humidity sensor records for date: {} = {}".format( test_date.strftime("%m/%d/%y"), len(recs)))
+print("\tAverrage: {} humidity".format(mean(recs)))
 # Module 5 code here:
